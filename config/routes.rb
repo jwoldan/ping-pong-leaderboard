2251aeb05adff7f278f58da9d0f1d32d81bc63ed
Jeffrey Woldan <jwoldan@gmail.com>
2017-01-26 14:26:53 -0500

    Update game#create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :games
   resources :games, only: [:new, :create]
 
   devise_for :users
   root to: "home#index"
   get '/history', to: 'home#history'
   get '/log',     to: 'games#new'
+  post '/log',    to: 'games#create'
 end
