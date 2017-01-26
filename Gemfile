f77319a0b15de6bb8fccef5eedeb57ad232a4276
Jeffrey Woldan <jwoldan@gmail.com>
2017-01-26 14:09:14 -0500

    Switch database to PostgreSQL

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'pg'
 gem 'rails',        '4.2.3'
 gem 'sqlite3'
 gem 'uglifier',     '>= 1.3.0'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170126185117) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.integer  "player_id",          null: false
     t.integer  "other_player_id",    null: false
@@ -23,8 +26,8 @@ ActiveRecord::Schema.define(version: 20170126185117) do
     t.datetime "updated_at",         null: false
   end
 
-  add_index "games", ["other_player_id"], name: "index_games_on_other_player_id"
-  add_index "games", ["player_id"], name: "index_games_on_player_id"
+  add_index "games", ["other_player_id"], name: "index_games_on_other_player_id", using: :btree
+  add_index "games", ["player_id"], name: "index_games_on_player_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",  null: false
@@ -43,8 +46,8 @@ ActiveRecord::Schema.define(version: 20170126185117) do
     t.integer  "games_played",           default: 0,   null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["rating"], name: "index_users_on_rating"
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["rating"], name: "index_users_on_rating", using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
