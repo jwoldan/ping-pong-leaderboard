6956cde6dd0e6f598842da6c5de31e3eb0b2a8f9
Jeffrey Woldan <jwoldan@gmail.com>
2017-01-26 12:42:15 -0500

    Update /log route to games#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
+  resources :games
   resources :games, only: [:new, :create]
 
   devise_for :users
   root to: "home#index"
   get '/history', to: 'home#history'
-  get '/log',     to: 'home#log'
+  get '/log',     to: 'games#new'
 end
