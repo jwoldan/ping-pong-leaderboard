25b33fdf2a70e7ffa98181264d5cd083216a9f8c
Jeffrey Woldan <jwoldan@gmail.com>
2017-01-26 14:38:03 -0500

    Add Game#try_set_date specs

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -15,5 +15,31 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:game) { FactoryGirl.create(:game) }
+
+
+  describe "#try_set_date" do
+
+    context "when passed an invalid date" do
+      it "does not update the date" do
+        date = game.date
+        game.try_set_date(2016, 2, 30)
+        expect(game.date).to eq(date)
+      end
+    end
+
+    context "when passed a valid date" do
+      it "updates the date" do
+        new_date = Faker::Date.between(5.years.ago, Date.today)
+        while game.date == new_date
+          new_date = Faker::Date.between(5.years.ago, Date.today)
+        end
+        game.try_set_date(new_date.year, new_date.month, new_date.day)
+        expect(game.date).to eq(new_date)
+      end
+    end
+
+  end
+
+  
 end
